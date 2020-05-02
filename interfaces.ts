2477991c0d1d816b61f5bd84c5766af58e5e90e4
Donovan <donovan@Donovan-MacBook-Pro.local>
2020-05-02 10:05:02 -0700

    added config options to constructor

---
+++ interfaces.ts
@@ -0,0 +1,18 @@
+interface BaseConfig {
+    user?: string;
+    database?: string;
+    password?: string;
+    port?: number;
+    host?: string;
+    connectionString?: string;
+}
+
+/**
+ * Debating if we want to have 'max' or not? Feel like we probably shouldnt place a cap.
+ * Or if we did, maybe that would signal that we need to spin up a new instance.
+ */
+export interface PoolManagerConfig extends BaseConfig {
+    min?: number,
+    // max?: number,
+    idleTimeout?: number,
+}
--- pool-mgr.ts
+++ pool-mgr.ts
@@ -1,53 +1,81 @@
-class PoolManager {
-    public initialConnex: number;
-    public static DEFAULT_CONNECTIONS: number = 5;
-    public connectionsInUse = {};
-    public allConnections = {};
-    public connectionsAvailable = {};
+import uuid from 'uuid';
+import { PoolManagerConfig } from './interfaces';
 
-    constructor(initialConnex?: number) {
-        this.initialConnex = initialConnex || PoolManager.DEFAULT_CONNECTIONS;
+export default class PoolManager {
+    private static DEFAULT_CONNECTIONS: number = 5;
+    private min: number;
+    private active = {};
+    private all = {};
+    private available = {};
+    private options: PoolManagerConfig;
+    private Client: any;
+
+    /**
+     * @param options PoolManagerConfig
+     * @param client the constructor of a postgres or mysql client or something similar.
+     */
+    constructor(options: PoolManagerConfig, client: any) {
+        this.options = options;
+        this.Client = client;
+        this.min = options.min || PoolManager.DEFAULT_CONNECTIONS;
+        this.openBatchConnections(this.min);
     }
+
+    /**
+     * This is the only public method because we want to encapsulate the pool mgr connecting logic.
+     * @param text SQL statement. Didn't go into detail on the different types of queries i could do.
+     * Focusing more on the implementation of the pool manager.
+     */
+    public query(text: string) {
+        const client = this.getConnection();
+        client.query(text, (err: Error, res: any) => {
+            if (err) {
+                throw err;
+            }
+            this.releaseConnection(client.key);
+            return res;
+        });
+    }
+
     private openBatchConnections(connex: number) {
         const newConnections = {};
-        for (let i = 0; i < connex; i++)  {
-            newConnections[this.createGuid()] = this.connected();
+        for (let i = 0; i < connex; i++) {
+            const connection = this.connect();
+            newConnections[connection.key] = connection;
         }
-        this.allConnections = Object.assign(this.allConnections, newConnections);
-        this.connectionsAvailable = newConnections;
+        this.all = Object.assign(this.all, newConnections);
+        this.available = newConnections;
     }
+
     private claimConnection() {
-        const keys = Object.keys(this.connectionsAvailable);
-        this.connectionsInUse[keys[0]] = this.connectionsAvailable[keys[0]];
-        delete this.connectionsAvailable[keys[0]];
-        return this.connectionsInUse[keys[0]];
+        const keys = Object.keys(this.available);
+        this.active[keys[0]] = this.available[keys[0]];
+        delete this.available[keys[0]];
+        return this.active[keys[0]];
     }
-    public getConnection() {
-        if (!(this.connectionsInUse < Object.keys(this.allConnections).length)) {
-            this.openBatchConnections(PoolManager.DEFAULT_CONNECTIONS);
+
+    private getConnection() {
+        if (!(this.active < Object.keys(this.all).length)) {
+            this.openBatchConnections(this.min);
         }
         return this.claimConnection();
     }
-    public releaseConnection(connectionKey: string) {
-        this.connectionsAvailable[connectionKey] = this.connectionsInUse[connectionKey];
-        delete this.connectionsInUse[connectionKey];
+
+    private releaseConnection(connectionKey: string) {
+        this.available[connectionKey] = this.active[connectionKey];
+        delete this.active[connectionKey];
         // minimum is 5
         // current connections in use = 17
         // available connections is = 40
     }
+
     private createGuid() {
-        return 'alskdfasdf';
+        return uuid.v4();;
     }
-    // private checkAvailableConnex() {
 
-    // }
-    private connected() {
-
+    private connect() {
+        return Object.assign(new this.Client(this.options), { key: this.createGuid() });
     }
 }
 
-// dependency injection to inject the actual database that we're going to use
-// pass in the env vars for the database to connect
 // initialize the pool manager on app start up
-// once we have a pool, we're ready to start receiving requests for connections
-
